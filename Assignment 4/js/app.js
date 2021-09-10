a7f67d0dd9cc29d75aaac925ce789b9bdeed0b54
Lorenzofly <89353421+Lorenzofly@users.noreply.github.com>
2021-09-10 10:12:46 -0500

    yes

--- Assignment 4/js/app.js
+++ Assignment 4/js/app.js
@@ -1,7 +1,15 @@
-$('#btsTest').click(function() {
+$('#btnTest').click(function() {
     const decTaxRate = .0925;
     let decHours = $('#txtHours').val();
     let decRate = $('#txtPayRate').val();
     console.log(decHours * decRate);
 });
-$('#txtPayRate')
+$('#cboEmployeeType').change(function() {
+    if($('#cboEmployeeType').val() == 'FULL'){
+        //$('#divHours').slideUp();
+        //$('#divHours').css('display','none');
+        $('#divHours').addClass('d-none');
+    }else {
+        $('#divHours').removeClass('d-none').slideDown();
+    }
+})
